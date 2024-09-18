8c196da623dfb185e20a8b4e7296347e38e04d2a
PY Liu <140623725+fraubentz@users.noreply.github.com>
2024-09-18 10:56:56 +0100

    Code and pass all tests of repeatString Completed with success, passing all 7 tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+//const repeatString = function() {
 
-};
+//};
+
+function repeatString(string, num) {
+    chosenString = "";
+    i = 0;
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    while (i < num) {
+        chosenString += string;
+        i++;
+    }
+    return chosenString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
