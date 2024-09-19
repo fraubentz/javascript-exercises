d747630950f464d1b99fffe77bc38e0db5971ad9
PY Liu <140623725+fraubentz@users.noreply.github.com>
2024-09-19 17:07:22 +0100

    Code and test moveFromArray Passed with success. Still need to review variadic functions and certain built-in methods for arrays.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...n) {
+    return list.filter(val => !n.includes(val));
+    //return filtered;
+    //let newArray = [];
+    //for (let i = 0; i < list.length; i++) {
+        //for (j of n) {
+            //if (list[i] !== j) {
+                //newArray.push(list[i]);
+            //}
+        //}
+    //}
+    //return newArray;
 };
 
 // Do not edit below this line
