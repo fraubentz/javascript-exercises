f5f1db6d637d3f809db89eb12a3dcd312974307b
PY Liu <140623725+fraubentz@users.noreply.github.com>
2024-09-29 15:20:25 +0100

    Code and test 05_sumAll program Completed with success!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num, num2) {
+    if (num < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(num) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+
+    if (num2 < num) {
+        const holder = num;
+        num = num2;
+        num2 = holder;
+    }
+
+    // init total sum
+    finalSum = 0;
+    // loop through integers starting from num and add integer to total
+    for (let i = num; i <= num2; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
