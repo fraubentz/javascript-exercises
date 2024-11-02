0890340fbbdce2672b8eb8cf09ef0f56dabc3dff
PY Liu <140623725+fraubentz@users.noreply.github.com>
2024-11-02 13:41:44 +0000

    Code and test palindromes.js Needed to check the solution for this one, as I used a too-complicated for-loop. Array methods worked the best.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
 
+const palindromes = function (string) {
+
+  const alphanumericalset = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+  const cleaned = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumericalset.includes(character))
+    .join('');
+
+  const rev = cleaned.split('').reverse().join('');
+
+  return cleaned === rev;
 };
 
 // Do not edit below this line
