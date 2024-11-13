8aba0f1d93d90efcb96c48bd5ee0dbbfa89d335c
PY Liu <140623725+fraubentz@users.noreply.github.com>
2024-11-13 11:44:12 +0000

    Code and test fibonacci.js Programmed and passed the test with success!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+let convertedNum;
+
+if (typeof num !== "number") {
+    convertedNum = parseInt(num);
+} else {
+    convertedNum = num;
+}
+
+let afterNum = 1;
+let prevNum = 0;
+
+if (convertedNum == 0) return 0;
+if (convertedNum < 0) return "OOPS";
+
+for (let i = 2; i <= convertedNum; i++) {
+    let summed = afterNum + prevNum;
+     prevNum = afterNum;
+     afterNum = summed;
+}
+
+return afterNum;
 
 };
 
