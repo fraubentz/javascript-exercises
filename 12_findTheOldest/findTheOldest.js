5fc3fcadeb96e3e4a4d4bdcbcc63dae1874d38c4
PY Liu <140623725+fraubentz@users.noreply.github.com>
2024-11-16 15:05:05 +0000

    Code and test findTheOldest.js Used reduce() array method to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const calcAge = function(birth, death){
+    if (!death) {
+        death = new Date().getFullYear();
+    }; 
+    return death - birth;
+};
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestPerson = calcAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentPerson = calcAge(current.yearOfBirth, current.yearOfDeath);
+        // returns condition ? value : value2
+        // condition is evaluated. If it's truthy then value is returned, otherwise value2.
+        // return oldestPerson < currentPerson ? current : oldest;
+
+        if (oldestPerson < currentPerson) {
+            return current;
+        } else {
+            return oldest;
+        }
+    });
 };
 
 // Do not edit below this line
